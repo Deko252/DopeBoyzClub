4b2c85107a9be220e28d0925d5f3469241abcfed
HYUNG MIN KIM <freshtuna88@hotmail.com>
2023-05-30 19:46:50 +0900

    반갑다게이들아

---
+++ src/main/java/com/example/dopeboyzclub/DopeBoyzClubApplication.java
@@ -0,0 +1,13 @@
+package com.example.dopeboyzclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopeBoyzClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DopeBoyzClubApplication.class, args);
+    }
+
+}
