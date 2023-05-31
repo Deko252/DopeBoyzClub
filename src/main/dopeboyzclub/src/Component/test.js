e29d80be6eadc9120cfe77d873c176963f188718
helloa1109 <helloa1109@naver.com>
2023-05-31 18:52:03 +0900

    jj

---
+++ src/main/dopeboyzclub/src/Component/test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Test(props) {
+    return (
+        <div>ㅎ
+            <div>
+                <h2>imddddddddfasdfasdfasgarsat4</h2>
+                <h1>asdfadsfdas</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Test;
